41067a7f9130807b12ef34e996611f2b0c4e1ece
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-11-10 21:31:48 -0500

    simple shader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { createScene, createStats } from './lib/create.js'
 
+const glslify = require('glslify')
+
+const loader = new THREE.OBJLoader()
+
 const { scene, camera, renderer } = createScene({
-  clearColor: 0x000000
+  clearColor: 0x3C4248
 })
 window.scene = scene
 
@@ -9,9 +13,11 @@ const light = new THREE.PointLight(0xffffff, 1, 1000)
 light.position.set(5, 10, 5)
 scene.add(light)
 
-camera.position.set(0,0,2)
+camera.position.set(0,4,4)
 
 const controls = new THREE.OrbitControls(camera, renderer.domElement)
+
+// === PLANE ===
 
 const planeMaker = () => {
   const geom = new THREE.PlaneGeometry(10, 10, 10, 10)
@@ -26,6 +32,40 @@ const planeMaker = () => {
 let plane = planeMaker()
 scene.add(plane)
 
+// === MATERIAL ===
+
+const uniforms = {
+  time: { type: 'f', value: 0.0, step: 0.03 }
+}
+
+const shaderMaterial = new THREE.ShaderMaterial({
+  uniforms: uniforms,
+  vertexShader: glslify('./shaders/vert.glsl'),
+  fragmentShader: glslify('./shaders/frag.glsl')
+})
+
+const mat = new THREE.MeshLambertMaterial({ color: 0xB9E13C })
+
+// === MESH ===
+
+const cmoMat = mat
+let cmo = null
+new THREE.JSONLoader().load('/models/cmo.json', (geometry) => {
+  const mesh = new THREE.Mesh(geometry, cmoMat)
+  cmo = mesh
+  mesh.position.set(0, 1.25, 0)
+
+  scene.add(mesh)
+})
+
+const update = (ts, delta) => {
+  uniforms.time.value += uniforms.time.step
+  if (cmo) {
+    cmo.rotation.y += delta * Math.PI / 16
+  }
+}
+
+const clock = new THREE.Clock()
 const stats = createStats()
 const render = (ts) => {
   stats.begin()
@@ -36,7 +76,7 @@ const render = (ts) => {
 
   renderer.render(scene, camera)
 
-  // update(ts)
+  update(ts, clock.getDelta())
   // uniforms.time.value += uniforms.time.step
 
   // capturer.capture(renderer.domElement)
---
+++ src/shaders/frag.glsl
@@ -0,0 +1,9 @@
+uniform float time;
+
+void main() {
+    float g = sin(time);
+    float r = cos(time);
+    float b = tan(time);
+
+    gl_FragColor = vec4(r, g, b, 1.0);
+}
