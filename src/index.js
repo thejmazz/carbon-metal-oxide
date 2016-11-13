1457f3979c7c4dd158a1f775319107d47848fdec
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-11-12 22:45:32 -0500

    clean

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import { createScene, createStats } from './lib/create.js'
 
 const glslify = require('glslify')
 
-// console.log(THREE.ShaderChunk.meshlambert_vert)
-// console.log(THREE.ShaderChunk.meshlambert_frag)
-
 const loader = new THREE.OBJLoader()
+const jsonLoader = new THREE.JSONLoader()
 
 const { scene, camera, renderer } = createScene({
   clearColor: 0x3C4248
@@ -35,55 +33,45 @@ const planeMaker = () => {
 let plane = planeMaker()
 scene.add(plane)
 
-// === MATERIAL ===
+// === CARBON METAL OXIDE ===
 
-const uniforms = Object.assign({}, THREE.ShaderLib.lambert.uniforms, {
-  time: { type: 'f', value: 0.0, step: 0.03 },
-})
+let cmo = null
 
-const shaderMaterial = new THREE.ShaderMaterial({
-  uniforms: uniforms,
-  vertexShader: glslify('./shaders/vert.glsl'),
-  fragmentShader: glslify('./shaders/frag.glsl'),
+const carbonMetalOxideMaterial = new THREE.ShaderMaterial({
+  uniforms: Object.assign({}, THREE.ShaderLib.lambert.uniforms, {
+    time: { type: 'f', value: 0.0, step: 0.03 },
+  }),
+  vertexShader: glslify('./shaders/cmo-vert.glsl'),
+  fragmentShader: glslify('./shaders/cmo-frag.glsl'),
   lights: true
 })
 
-const mat = new THREE.MeshLambertMaterial({ color: 0xB9E13C })
-
-// === MESH ===
-
-const cmoMat = shaderMaterial
-let cmo = null
-new THREE.JSONLoader().load('/models/cmo.json', (geometry) => {
-  const mesh = new THREE.Mesh(geometry, cmoMat)
+jsonLoader.load('models/cmo.json', (geometry) => {
+  const mesh = new THREE.Mesh(geometry, carbonMetalOxideMaterial)
   cmo = mesh
   mesh.position.set(0, 1.25, 0)
 
   scene.add(mesh)
 })
 
+// === LOOP ===
+
 const update = (ts, delta) => {
-  uniforms.time.value += uniforms.time.step
+  carbonMetalOxideMaterial.uniforms.time.value += carbonMetalOxideMaterial.uniforms.time.step
   if (cmo) {
     cmo.rotation.y += delta * Math.PI / 16
   }
 }
+
+// === RENDER ===
 
 const clock = new THREE.Clock()
 const stats = createStats()
 const render = (ts) => {
   stats.begin()
 
-  // for (let keyframe of keyframes) {
-  //   keyframe()
-  // }
-
   renderer.render(scene, camera)
-
   update(ts, clock.getDelta())
-  // uniforms.time.value += uniforms.time.step
-
-  // capturer.capture(renderer.domElement)
 
   stats.end()
 
